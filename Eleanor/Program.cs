b55bc2fcb0c84fa506732c0d6329b50e8be095d2
Allyson Alves de Lima <allysonalvesdelima@gmail.com>
2026-02-08 18:53:27 -0300

    chore: corrige nomenclatura de pastas, arquivos e namespaces

--- Eleanor/Program.cs
+++ Eleanor/Program.cs
@@ -3,3 +3,5 @@ using static Infra.DependencyInjection.InfrastructureServiceCollectionExtensions
 
 var builder = Host.CreateApplicationBuilder();
 builder.Services.AdicionarContext(builder.Configuration);
+
+//nome do projeto em letra minúscula 
